4cd4f6c6b00d43473b7d7742db6bb06ff9b5c4e5
Kevin Bowling <kevin.bowling@kev009.com>
2010-11-27 16:14:22 -0700

    More httpd work: add htdocs to dist tarball, small change to list

--- src/httpd.c
+++ src/httpd.c
@@ -109,6 +109,7 @@ json_request_cb(struct evhttp_request *req, void *arg)
 
   struct PL_entry *player;
   pthread_rwlock_rdlock(&PL_rwlock);
+  evbuffer_add_printf(bout, "\n    <h2>%d users online</h2>\n", PL_count);
   SLIST_FOREACH(player, &PL_head, PL_entries)
   {
     evbuffer_add_printf(bout, "    %s - %s<br>\n", player->username.str, 
