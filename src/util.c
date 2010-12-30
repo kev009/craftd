1bbdb0764ea186bc1bbd5c46fc66587a69394678
Kevin Bowling <kevin.bowling@kev009.com>
2010-12-30 01:29:35 -0700

    Add a generic itoa() function to use for base36 NBT worldfile loading

--- src/util.c
+++ src/util.c
@@ -181,6 +181,46 @@ bstring getMCString(struct evbuffer *buf, int16_t len)
 }
 
 /**
+ * Take an integer and convert it to a base 2 to 36 char representation.
+ *
+ * Ideas from (http://www.jb.man.ac.uk/~slowe/cpp/itoa.html) and K&R pg. 64
+ *
+ * @param value integer to convert
+ * @param result string representation
+ * @param base conversion base, 2 to 36
+ * @return pointer to string representation
+ */
+char *itoa(int value, char *result, int base)
+{
+  int tmp_value;
+  char *ptr = result;
+  char *ptr1 = result, tmp_char;
+
+  assert(base > 2 || base < 36);
+
+  do
+  {
+    tmp_value = value;
+    value /= base;
+    *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrs"
+             "tuvwxyz" [35 + (tmp_value - value * base)];
+  }
+  while (value);
+
+  /* Add a negative sign if needed */
+  if(tmp_value < 0)
+    *ptr++ = '-';
+  *ptr-- = '\0';
+  while (ptr1 < ptr)
+  {
+    tmp_char = *ptr;
+    *ptr-- = *ptr1;
+    *ptr1++ = tmp_char;
+  }
+  return result;
+}
+
+/**
  * Simple calloc wrapper w/error handling
  *
  * @param num number of objects to allocate
