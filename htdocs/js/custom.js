25f94d76f8b9ddf4b959d777e70077aa07a68a9d
Coestar <coe@coesquest.com>
2011-02-04 17:46:09 -0700

    Incorporated JQuery UI, added new images, various CSS improvements

---
+++ htdocs/js/custom.js
@@ -0,0 +1,13 @@
+// Custom JQuery/Javascript
+
+$(document).ready(function() {
+
+    $("tbody tr:even").addClass("alt");
+    $("#output li:even").addClass("alt");
+    $("#outwrap").resizable({ handles: 's' });
+
+    // Scroll to bottom of console output
+    var objDiv = document.getElementById("output");
+    objDiv.scrollTop = objDiv.scrollHeight;
+
+});
