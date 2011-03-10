d9c2ce1e51f2d27196aefd8abce2248cfcb3c1b9
jbergstroem <bugs@bergstroem.nu>
2011-03-10 21:50:07 +0100

    Add external jansson library to build process

--- extras/build.sh
+++ extras/build.sh
@@ -7,7 +7,8 @@ DATADIR=$CRAFTDDIR/data
 EXAMPLESDIR=$CRAFTDDIR/examples
 LIBEVENT2VER=libevent-2.0.10-stable
 LIBEVENT2DIR=$SRCDIR/$LIBEVENT2VER
-
+JANSSONVER=jansson-2.0
+JANSSONDIR=$SRCDIR/$JANSSONVER
 
 echo "Creating directories..."
 mkdir -p $SRCDIR $LIBDIR $DATADIR $EXAMPLESDIR|| exit 1
@@ -23,15 +24,29 @@ cd $LIBEVENT2DIR
 make || exit 1
 cp .libs/*.so* $LIBDIR/ || exit 1
 
+cd $SRCDIR
+echo "Downloading jansson..."
+wget http://www.digip.org/jansson/releases/$JANSSONVER.tar.gz || exit 1
+tar zxvf $JANSSONVER.tar.gz || exit 1
+
+echo "Building jansson..."
+cd $JANSSONVER
+./configure || exit 1
+make || exit 1
+cp src/.libs/*.so* $LIBDIR/ || exit 1
+
 echo "Cloning craftd Git repository..."
 cd $SRCDIR
-git clone git://github.com/kev009/craftd.git || exit 1
+git clone git://github.com/jbergstroem/craftd.git || exit 1
 
 echo "Building craftd..."
 cd craftd
 autoreconf -i || exit 1
 export libevent2_pthreads_LIBS="-levent -levent_pthreads -L$LIBDIR"
 export libevent2_pthreads_CFLAGS="-I$LIBEVENT2DIR/include"
+export jansson_LIBS="-ljansson -L$LIBDIR"
+export jansson_CFLAGS="-I$JANSSONDIR/include"
+
 ./configure --prefix=$CRAFTDDIR --bindir=$CRAFTDDIR --sysconfdir=$EXAMPLESDIR --datarootdir=$DATADIR || exit 1
 make || exit 1
 make install || exit 1
