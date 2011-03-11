5d0eed09e2d0181385a09353eb609e6e52ad6fe0
Kevin Bowling <kevin.bowling@kev009.com>
2011-03-10 18:46:22 -0700

    Remove icky LD_LIBRAY_PATH usage in build.sh with a relative -rpath

--- extras/build.sh
+++ extras/build.sh
@@ -51,6 +51,9 @@ export libevent2_pthreads_CFLAGS="-I$LIBEVENT2DIR/include"
 export jansson_LIBS="-ljansson -L$LIBDIR"
 export jansson_CFLAGS="-I$JANSSONDIR/src"
 
+# rpath magic to find the local libs
+export LDFLAGS="$LDFLAGS -Wl,-rpath,\$$ORIGIN/lib"
+
 ./configure -q --prefix=$CRAFTDDIR --bindir=$CRAFTDDIR --sysconfdir=$EXAMPLESDIR --datarootdir=$DATADIR || exit 1
 make >/dev/null 2>&1 || exit 1
 make install >/dev/null 2>&1 || exit 1
@@ -62,5 +65,5 @@ cd $CRAFTDDIR
 echo "Done"
 echo
 echo "--------------------------------------------------"
-echo "Start craftd with: LD_LIBRARY_PATH=lib ./craftd"
+echo "Start craftd with: ./craftd"
 echo "--------------------------------------------------"
