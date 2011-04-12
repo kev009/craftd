6b1b02d8b7379504af754aa71f03cc0980ea14a1
meh <meh@paranoici.org>
2011-04-12 22:09:06 +0200

    Fix bug with libconfig < 1.4

--- src/extras.c
+++ src/extras.c
@@ -34,8 +34,11 @@ config_export (config_setting_t* setting, config_t* config)
 
     memset(config, 0, sizeof(*config));
 
-    config->root      = setting;
+    config->root = setting;
+
+    #if LIBCONFIG_VER_MAJOR >= 1 && LIBCONFIG_VER_MINOR >= 4
     config->tab_width = 2;
+    #endif
 
     return CONFIG_TRUE;
 }
