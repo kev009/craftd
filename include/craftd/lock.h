bc740ccfeea5d1e1f8e83d7c6a96303c001745bc
Kevin Bowling <kevin.bowling@kev009.com>
2011-04-21 19:28:28 -0700

    Stub pthread_spinlock_t for Cygwin and OpenBSD

--- include/craftd/lock.h
+++ include/craftd/lock.h
@@ -23,71 +23,12 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef CRAFTD_COMMON_H
-#define CRAFTD_COMMON_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdarg.h>
-#include <string.h>
-#include <limits.h>
-
-#include <assert.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-
 #include <pthread.h>
 
-#define ARRAY_SIZE(array) (sizeof(array) / sizeof(*array))
-
-#define DO \
-    for (char __cddo_tmp__ = 0; __cddo_tmp__ == 0; __cddo_tmp__++)
-
-#include <event2/event.h>
-#include <event2/buffer.h>
-#include <event2/bufferevent.h>
-#include <event2/listener.h>
-#include <event2/thread.h>
-
-#include <craftd/config.h>
-
-#if (SIZEOF_FUNCTION_POINTER == 4 && SIZEOF_POINTER == 4) || (SIZEOF_FP == 4 && SIZEOF_INTPTR_T == 4)
-    typedef int32_t CDPointer;
-#else
-    typedef int64_t CDPointer;
-#endif
-
-#ifdef __cplusplus
-#   define PUBLIC extern "C"
-#else
-#   define PUBLIC extern
-#endif
-
-#define CDNull (0)
-
-#include <craftd/lock.h>
-#include <craftd/utils.h>
-#include <craftd/memory.h>
-#include <craftd/extras.h>
-
-#include <craftd/Error.h>
-#include <craftd/Arithmetic.h>
-#include <craftd/List.h>
-#include <craftd/Map.h>
-#include <craftd/Hash.h>
-#include <craftd/Set.h>
-#include <craftd/String.h>
-#include <craftd/Regexp.h>
-#include <craftd/Dynamic.h>
-
-#include <craftd/javaendian.h>
-
-#include <craftd/Buffer.h>
-#include <craftd/Buffers.h>
-
+#ifndef HAVE_PTHREAD_SPIN_T
+#define pthread_spinlock_t      pthread_mutex_t
+#define pthread_spin_init(l, a)	pthread_mutex_init(l, NULL)
+#define pthread_spin_destroy(l)	pthread_mutex_destroy(l)
+#define pthread_spin_lock(l)	pthread_mutex_lock(l)
+#define pthread_spin_unlock(l)	pthread_mutex_unlock(l)
 #endif
